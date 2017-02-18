3aa829ca2250a216b6ff02cedba9258a9c0609a8
ankit24007 <ankit24007@gmail.com>
2017-02-18 19:09:46 +0530

    get messages api

--- server.js
+++ server.js
@@ -1,8 +1,11 @@
 var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
-var mongo = require('mongodb').MongoClient;
-var database;
+var mongoose = require('mongoose');
+
+var Message = mongoose.model('Message', {
+	msg: String
+});
 
 app.use(bodyParser.json());
 
@@ -12,18 +15,28 @@ app.use(function(req,res,next){
 	next();
 })
 
+
+mongoose.connect("mongodb://localhost:27017/test", function(err,db){
+	if(!err){
+		console.log("We are connected to mongo");
+	}
+})
+
+app.get('/api/message', GetMessages);
+
 app.post('/api/message', function(req,res) {
 	console.log(req.body);
-	database.collection('messages').insertOne(req.body);
+	//database.collection('messages').insertOne(req.body);
+	var message = new Message(req.body);
+	message.save();
 	res.status(200);
 })
 
-mongo.connect("mongodb://localhost:27017/test", function(err,db){
-	if(!err){
-		console.log("We are connected to mongo");
-		database = db;
-	}
-})
+function GetMessages(req, res){
+	Message.find({}).exec(function(err, result){
+		res.send(result);
+	})
+}
 
 var server = app.listen(5000, function() {
 	console.log('listening on port', server.address().port);
